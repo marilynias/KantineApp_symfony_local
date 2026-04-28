72a2100e9a8ee8680aaee11d761290cd6e3fc34d
Marilynias <marilynia57@gmail.com>
2026-04-28 10:18:49 +0200

    formatting

--- apps/zeiterfassung/src/Controller/TimeEntryBatchController.php
+++ apps/zeiterfassung/src/Controller/TimeEntryBatchController.php
@@ -34,55 +34,41 @@ final class TimeEntryBatchController extends AbstractController
         $data = [];
         $data1 = [];
 
+        // sort entries into [Costumer][Month][entiry]
         foreach ($selectedUsers as $timeEntry) {
             if(!$timeEntry instanceof TimeEntry) continue;
             $month = $timeEntry->getCheckinTime()->format('MM.yyyy');
             $data[$timeEntry->getUser()->getFullname()][$month][] = [
-                // 'Datum'=>   $format, $timeEntry->getCheckinTime()->format('ll dd.mm.yyyy'), 
                 'Datum'=>   datefmt_format($format, $timeEntry->getCheckinTime()), 
                 'Eintrag' => $timeEntry->getCheckinTime()->format('h:m'), 
                 'Austrag' => $timeEntry->getCheckoutTime()? $timeEntry->getCheckoutTime()->format('h:m'):''];
-            // $data[] = [
-            //     'Vorname' => $user->getFirstname(),
-            //     'Nachname' => $user->getLastname(),
-            // ];
         }
 
         $zipName = tempnam(sys_get_temp_dir(), 'zip_');
-        
         $zip = new ZipArchive();
-        
-        
         if ($zip->open($zipName, ZipArchive::CREATE) !== true) {
             throw new \RuntimeException(_('Cannot open ' . $zipName));
         }
 
+        // write into zip archive structured costumer/month.xlsx
         foreach ($data as $costumer => $months) {
-            // if(!$costumer instanceof Costumer) continue;
-            
             $zip->addEmptyDir($costumer);
             foreach ($months as $entries) {
-                //     $writer->write($value);
                 $source = new ArraySourceIterator($entries);
                 $tmpName = tempnam(sys_get_temp_dir(), 'xlsx_');
-                if(file_exists($tmpName)) unlink($tmpName);             // hacky way to just get a random name
+                if(file_exists($tmpName)) unlink($tmpName);             // hacky way to just get a random name, not the new file
                 $writer = new XlsxWriter($tmpName);
                 Handler::create($source, $writer)->export();
             }
-            // $writer->close();
             $zip->addFile($tmpName, $costumer.DIRECTORY_SEPARATOR.$costumer.'_'.date('m.Y').'.xlsx');
         }
         if (!$zip->close()) throw new \RuntimeException(_('Cannot close ' . $zipName));
+
+
         $response = new BinaryFileResponse($zipName);
         $response->headers->set('Content-Type', 'application/zip');
         $response->setContentDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT, 'Teilnehmer_' . date('m.Y') . '.zip');
         $this->addFlash('sonata_flash_success', 'successfully exported');
         return $response;
-        // return new StreamedResponse(function () use ($sources, $writer) {
-            
-        // }, 200, [
-        //     'Content-Type' => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-        //     'Content-Disposition' => 'attachment; filename="' . $fileName . '"',
-        // ]);
     }
 }
